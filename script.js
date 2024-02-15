03dc69bf1376d268237065ae8239dd4f9caa245d
raeez21 <mohammadraeez.mec@gmail.com>
2024-02-15 11:47:24 +0530

    Adding starting project

---
+++ script.js
@@ -0,0 +1,11 @@
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var css = document.querySelector("h3");
+var body = document.getElementById("gradient")
+function setGradient(){
+    body.style.background = "linear-gradient(to right, "+color1.value+ "," +color2.value+")";
+    css.textContent = body.style.background
+}
+color1.addEventListener("input",setGradient)
+css.textContent = body.style.background+";"
+color2.addEventListener("input",setGradient)
